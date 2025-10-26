b903ba8148063dd217f59782f5a479d96f0bec8a
MaheeGamage <mahee.gamage@gmail.com>
2025-10-27 00:13:08 +0200

    mui signin example page added

--- learner-web-app/src/routes.ts
+++ learner-web-app/src/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
   route("/login", "./pages/LoginPage.tsx"),
+  route("/temp/signin", "./other/SignIn.tsx"),
   // * matches all URLs, the ? makes it optional so it will match / as well
   route("*?", "catchall.tsx"),
 ] satisfies RouteConfig;
